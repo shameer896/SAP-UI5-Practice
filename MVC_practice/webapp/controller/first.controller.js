60534b80de2238c39ef38877165f460822ce9769
Shameer Abdul Kader <62144206+shameer896@users.noreply.github.com>
2025-03-04 16:45:18 +0530

    ui5 freestyle app via template generator

---
+++ MVC_practice/webapp/controller/first.controller.js
@@ -0,0 +1,6 @@
+sap.ui.define(["sap/ui/core/Controller"],function(Controller){
+    "use strict";
+    return Controller.extend("shameer.mvcapp.controller.first",{
+        
+    })
+})
---
+++ demo_project1/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["sak/demoproject1/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
